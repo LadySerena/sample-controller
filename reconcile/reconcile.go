8a449fcb94cbc4e695f8d92e408240404a7c6f26
LadySerena <serena.tiede+github@gmail.com>
2021-07-23 21:15:20 -0500

    fix: ignore not found errors for pods
    Since the controller could get an event but the cache is missing the pod
    it's probably that there was a lag between the event firing and the
    cache knowing that the pod is already gone. If it was meant to be the
    pod will come back.

--- reconcile/reconcile.go
+++ reconcile/reconcile.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-type Loop struct{
+type Loop struct {
 	Client client.Client
-	Log logr.Logger
+	Log    logr.Logger
 }
 
 func (l *Loop) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
@@ -20,8 +21,9 @@ func (l *Loop) Reconcile(ctx context.Context, request reconcile.Request) (reconc
 
 	getPodErr := l.Client.Get(ctx, request.NamespacedName, &pod)
 	if getPodErr != nil {
-		// TODO remove not found pods from queue
-		// error package here (need to bookmark this) https://pkg.go.dev/k8s.io/apimachinery/pkg/api/errors
+		if errors.IsNotFound(getPodErr) {
+			return reconcile.Result{}, nil // don't return error since it's probably it's cache sync issue
+		}
 		log.Error(getPodErr, "could not get pod")
 		return reconcile.Result{}, getPodErr
 	}
