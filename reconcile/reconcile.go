ed41863558ce3df2e1acb69a7fe8d4184e478d98
LadySerena <serena.tiede+github@gmail.com>
2021-07-24 15:47:04 -0500

    feat: add annotation based filtering
    The controller will only annotate pods if they have the "timestamp"="true"
    annotation. Other pods without that annotation will pass by unbothered.

--- reconcile/reconcile.go
+++ reconcile/reconcile.go
@@ -2,6 +2,7 @@ package reconcile
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
@@ -10,11 +11,18 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
+const (
+	timestampKey = "creation"
+	selectionKey = "timestamp"
+)
+
+// Loop is the struct that implements the reconcile.Reconciler interface in controller runtime
 type Loop struct {
 	Client client.Client
 	Log    logr.Logger
 }
 
+// Reconcile is where the logic for the reconcilliation loop happens. In this case it is annotating and logging timestamps of pod creation
 func (l *Loop) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
 	log := l.Log.WithValues("pod", request.NamespacedName)
 	var pod v1.Pod
@@ -27,15 +35,35 @@ func (l *Loop) Reconcile(ctx context.Context, request reconcile.Request) (reconc
 		log.Error(getPodErr, "could not get pod")
 		return reconcile.Result{}, getPodErr
 	}
-	log.Info("pod is here", "pod name", request.Name, "timestamp of creation", pod.Status.StartTime.Time.String())
-	if pod.Annotations == nil {
-		pod.Annotations = map[string]string{}
+	shouldAnnotate, checkErr := annotatePod(pod) // check if we should annotate the pod
+	if checkErr != nil {
+		log.Error(checkErr, "could not check annotations of pod skipping", "pod name", request.Name)
+		return reconcile.Result{}, checkErr
 	}
-	pod.Annotations["creation"] = pod.Status.StartTime.Time.String()
-	updateErr := l.Client.Update(ctx, &pod)
-	if updateErr != nil {
-		log.Error(updateErr, "could not update timestamp")
-		return reconcile.Result{}, updateErr
+	if shouldAnnotate {
+		// if we should annotate then log the pod name and the creation timestamp
+		log.Info("pod is here", "pod name", request.Name, "timestamp of creation", pod.Status.StartTime.Time.String())
+		// only add the timestamp if it isn't already there
+		_, keyPresent := pod.Annotations[timestampKey]
+		if !keyPresent {
+			pod.Annotations[timestampKey] = pod.Status.StartTime.Time.String()
+			updateErr := l.Client.Update(ctx, &pod)
+			if updateErr != nil {
+				log.Error(updateErr, "could not update timestamp")
+				return reconcile.Result{}, updateErr
+			}
+		}
 	}
 	return reconcile.Result{}, nil
+}
+
+func annotatePod(pod v1.Pod) (bool, error) {
+	if pod.Annotations == nil {
+		return false, nil
+	}
+	value, isPresent := pod.Annotations[selectionKey]
+	if !isPresent {
+		return false, nil
+	}
+	return strconv.ParseBool(value)
 }
